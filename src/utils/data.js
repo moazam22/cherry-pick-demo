097351a1e88bc351f04e1eefb04a0e19604f9fa7
moazam22 <moazamrana22@gmail.com>
2023-05-02 15:56:45 +0500

    mock data added in utils

---
+++ src/utils/data.js
@@ -0,0 +1,24 @@
+const data=[
+  {
+    name: 'Rana Moazam',
+    email: 'moazamrana22@gmail.com',
+    age: 26,
+  },
+  {
+    name: 'John Doe',
+    email: 'moazamrana22@gmail.com',
+    age: 22,
+  },{
+    name: 'John Smith',
+    email: 'moazamrana22@gmail.com',
+    age: 29,
+  },{
+    name: 'John Wick',
+    email: 'moazamrana22@gmail.com',
+    age: 33,
+  },{
+    name: 'Test User',
+    email: 'moazamrana22@gmail.com',
+    age: 45,
+  },
+];
